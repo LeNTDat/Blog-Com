e41bf7c7a35120693d44293272a5f749dafbc596
Dat <takexa123456@gmail.com>
2021-05-20 22:11:39 +0700

    mainpage header and body content builder

---
+++ my-blog/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Mainpage from "./compopnent/client/mainpage";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <>
+      <Mainpage />
+    </>
+  );
+}
+
+export default App;
